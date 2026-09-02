338229e6e95c3c63c1d50be479f1fedd1d4a59ab
Sonykhan1121 <Sonykhan1121@users.noreply.github.com>
2026-09-02 04:46:19 +0000

    deploy: 239971bfcd924f359ec82e4f0b5a8517f290f762

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "801670669"
+    serviceWorkerVersion: "3706095292"
   }
 });
