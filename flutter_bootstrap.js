32e53208c881e2af9e21b38b144bb6a41abb9c72
Sonykhan1121 <Sonykhan1121@users.noreply.github.com>
2026-09-05 10:39:31 +0000

    deploy: 39db62d0985fc29ae543fa3633839ab7aee7b370

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -13,7 +13,7 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 // never waits for portfolio photos: they are requested by visible widgets.
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1565693986"
+    serviceWorkerVersion: "644466293"
   },
   onEntrypointLoaded: async function(engineInitializer) {
     try {
